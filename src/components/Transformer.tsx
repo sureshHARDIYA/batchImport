c266f17331c7d7ba1eb881f0497b2fac9057fcfb
Suresh KUMAR Mukhiya <itsmeskm99@gmail.com>
2022-03-14 09:39:18 +0100

    Update Transformer.tsx

--- src/components/Transformer.tsx
+++ src/components/Transformer.tsx
@@ -1,6 +1,6 @@
+import { useMemo } from "react";
 import { Form, Input, Button, Space, Select } from "antd";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { useMemo } from "react";
 
 const getOperators = [
   { label: "includes", value: "includes" },
@@ -14,14 +14,6 @@ const Transformer = ({ columnNames }: any) => {
     console.log("Received values of form:", values);
   };
 
-  const handleChange = () => {
-    form.setFieldsValue({ column: [] });
-  };
-
-  const handleOperatorChange = () => {
-    form.setFieldsValue({ operator: [] });
-  };
-
   const getColumns = useMemo(() => {
     return columnNames.map((col: any) => {
       return {
@@ -33,73 +25,74 @@ const Transformer = ({ columnNames }: any) => {
 
   return (
     <>
-      <Form
-        name="dynamic_form_nest_item"
-        onFinish={onFinish}
-        autoComplete="off"
-        form={form}
-      >
-        <Form.List name="users">
-          {(fields, { add, remove }) => (
-            <>
-              {fields.map(({ key, name, ...restField }) => (
-                <Space
-                  key={key}
-                  style={{ display: "flex", marginBottom: 8 }}
-                  align="baseline"
+      <Form form={form} name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
+      <Form.List name="rules">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map(field => (
+              <Space key={field.key} align="baseline">
+                <Form.Item
+                  noStyle
+                  shouldUpdate={(prevValues, curValues) =>
+                    prevValues.area !== curValues.area || prevValues.sights !== curValues.sights
+                  }
                 >
-                  All Rows <em>WHERE</em>
-                  <Form.Item
-                    {...restField}
-                    name="column"
-                    label="Column"
-                    rules={[
-                      { required: true, message: "Missing Column Names" },
-                    ]}
-                    style={{ minWidth: "150px" }}
-                  >
-                    <Select options={getColumns} />
-                  </Form.Item>
-                  <Form.Item
-                    {...restField}
-                    name="operator"
-                    label="Operator"
-                    rules={[
-                      { required: true, message: "Missing Operator Names" },
-                    ]}
-                    style={{ minWidth: "150px" }}
-                  >
-                    <Select options={getOperators} />
-                  </Form.Item>
-                  <Form.Item
-                    {...restField}
-                    name={[name, "last"]}
-                    rules={[{ required: true, message: "Missing last name" }]}
-                  >
-                    <Input placeholder="Last Name" />
-                  </Form.Item>
-                  <MinusCircleOutlined onClick={() => remove(name)} />
-                </Space>
-              ))}
-              <Form.Item>
-                <Button
-                  type="dashed"
-                  onClick={() => add()}
-                  block
-                  icon={<PlusOutlined />}
+                  {() => (
+                    <Form.Item
+                      {...field}
+                      label="Column Names"
+                      name={[field.name, 'columnName']}
+                      rules={[{ required: true, message: 'Missing Column Names' }]}
+                    >
+                      <Select style={{ width: 130 }} options={getColumns}>
+                      </Select>
+                    </Form.Item>
+                  )}
+                </Form.Item>
+                <Form.Item
+                  noStyle
+                  shouldUpdate={(prevValues, curValues) =>
+                    prevValues.area !== curValues.area || prevValues.sights !== curValues.sights
+                  }
                 >
-                  Add field
-                </Button>
-              </Form.Item>
-            </>
-          )}
-        </Form.List>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+                  {() => (
+                    <Form.Item
+                      {...field}
+                      label="Operator"
+                      name={[field.name, 'operator']}
+                      rules={[{ required: true, message: 'Missing Operator' }]}
+                    >
+                      <Select style={{ width: 130 }} options={getOperators}>
+                      </Select>
+                    </Form.Item>
+                  )}
+                </Form.Item>
+                <Form.Item
+                  {...field}
+                  label="Price"
+                  name={[field.name, 'price']}
+                  rules={[{ required: true, message: 'Missing price' }]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <MinusCircleOutlined onClick={() => remove(field.name)} />
+              </Space>
+            ))}
+            <Form.Item>
+              <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                Add Transformation Rule
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     </>
   );
 };
